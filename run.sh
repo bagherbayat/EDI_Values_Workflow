0bae575faf1ba883ef0d358b24d06476598470d1
Bagher Bayat <48024686+bagherbayat@users.noreply.github.com>
2020-10-21 19:28:15 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ mkdir Water_Stress_Maps_jpg
 mkdir Water_Stress_Maps_GTiff
 mkdir Water_Stress_Maps_CSV
 Rscript EDI_Values_Workflow_VLab.R
-zip -r archivename.zip Water_Stress_Maps_jpg
-zip -r archivename.zip Water_Stress_Maps_GTiff
-zip -r archivename.zip Water_Stress_Maps_CSV
+zip -r Water_Stress_Maps_jpg.zip Water_Stress_Maps_jpg
+zip -r Water_Stress_Maps_GTiff.zip Water_Stress_Maps_GTiff
+zip -r Water_Stress_Maps_CSV.zip Water_Stress_Maps_CSV
