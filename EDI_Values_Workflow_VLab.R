0838ef6a27211c8ddb0d16f21fd03fe94d659819
Bagher Bayat <48024686+bagherbayat@users.noreply.github.com>
2020-10-22 23:11:23 +0200

    Update EDI_Values_Workflow_VLab.R

--- EDI_Values_Workflow_VLab.R
+++ EDI_Values_Workflow_VLab.R
@@ -1,7 +1,7 @@
 ### EDI_Values_Workflow:
 #This workflow quantifies one decade of (agricultural) water stress levels across Europe using satellite-derived Evapotranspiration (ET) data sets and Evaporative Drought Index (EDI) values
 
-##Authors: Bagher Bayat (b.bayat@fz-juelich.de and bagher.bayat@gmail.com) and Carsten Montzka (c.montzka@fz-juelich.de)
+##Authors: Bagher Bayat (b.bayat@fz-juelich.de and bagher.bayat@gmail.com), Carsten Montzka (c.montzka@fz-juelich.de), Harry Vereecken (h.vereecken@fz-juelich.de)
 #Institute of Bio- and Geosciences: Agrosphere (IBG-3), Forschungszentrum Jülich GmbH, 52425 Jülich, Germany
 #Date:  20 October 2020
 
